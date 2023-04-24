b2d77ff7cb089dcfe40c55e12c86c847ad98f5cf
Svenson-Maximus <Sv3nleutenegger@gmail.com>
2023-04-24 14:27:15 +0200

    Basci Homepage frontend #closes 8

---
+++ frontend/src/pages/Home.svelte
@@ -0,0 +1,21 @@
+<div class="gradient-background" />
+<div class="container">
+    <div class="playerInfo">
+        <img alt="robohash" src="https://robohash.org/YOUR-TEXT.png" />
+    </div>
+    <div class="levels">
+        <!-- Levels here -->
+    </div>
+</div>
+
+<style>
+    .playerInfo {
+        flex: 1;
+        padding: 1rem;
+    }
+
+    .levels {
+        flex: 2;
+        padding: 1rem;
+    }
+</style>
---
+++ frontend/src/routes.js
@@ -0,0 +1,10 @@
+import Home from "./pages/Home.svelte";
+import Jobs from "./pages/Jobs.svelte";
+
+export default {
+    '/': Home,
+    '/home': Home,
+    
+
+    '/jobs': Jobs
+}
